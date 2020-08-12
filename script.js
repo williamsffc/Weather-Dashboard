62a5f8713f8596db3b96f1334fe8faebdf92b4b0
will.i.am <67477328+williamsffc@users.noreply.github.com>
2020-08-12 09:19:09 -0700

    added 5 day

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ $(document).ready(function () {
         var inputCity = $("#input").val();
         var key = "64b15ee1fcd8bf94f299ecb4565feacd";
         var todayDate = moment().format("M/DD/YYYY");
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + key + "&units=imperial";
-        // console.log(queryURL);
+        var queryURl_current = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + key + "&units=imperial";
+        var queryURL_5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + key + "&units=imperial&cnt=5";
+
         $.ajax({
-            url: queryURL,
+            url: queryURl_current,
             method: "GET",
         }).then(function (result) {
             // console.log(result);
@@ -17,28 +18,24 @@ $(document).ready(function () {
             $("#current").append("<p>Humidity: " + result.main.humidity + "%</p>")
             $("#current").append("<p>Wind Speed: " + result.wind.speed + " MPH</p>")
             $("#current").append("<p>UV Index: " + "___</p>")
-        });    
-    });
-
-
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + key + "&units=imperial";
-
-        console.log(queryURL);
-
+        });  
+        
         $.ajax({
-            url: queryURL,
+            url: queryURL_5Days,
             method: "GET",
-
-        }).then(function (result) {
-            console.log(result);
-
-            // $("#current").append("<p>" + result.name + " " + todayDate + "___</p>")
-            // $("#current").append("<p>Temperature: " + result.main.temp + " &#176F</p>")
-            // $("#current").append("<p>Humidity: " + result.main.humidity + "%</p>")
-            // $("#current").append("<p>Wind Speed: " + result.wind.speed + " MPH</p>")
-            // $("#current").append("<p>UV Index: " + "___</p>")
+        }).then(function (res) {
+            // console.log(res);
+            // $(".five_forecast").append("<h1>5-Day Forecast:" + "</h1>");
+            $(".day_1").append("<p>"+ todayDate + "</p>")
+            $(".day_1").append("<p>img</p>")
+            $(".day_1").append("<p>Temp: " + res.list[0].main.temp + "&#176F</p>")
+            $(".day_1").append("<p>Humidity: " + res.list[0].main.humidity + "%</p>")
         });
+        
 
- 
+
+
+
+    });
 
 })
