09263844e1baef97fb3472cba6513f938a1757b5
will.i.am <67477328+williamsffc@users.noreply.github.com>
2020-08-13 07:45:47 -0700

    added pics

--- script.js
+++ script.js
@@ -7,31 +7,53 @@ $(document).ready(function () {
         var todayDate = moment().format("M/DD/YYYY");
         var queryURl_current = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + key + "&units=imperial";
         var queryURL_5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + key + "&units=imperial&cnt=5";
+        var queryURL_UV = "https://api.openweathermap.org/data/2.5/uvi?q=" + inputCity + "&appid=" + key + "&units=imperial";
 
+       
         $.ajax({
             url: queryURl_current,
             method: "GET",
         }).then(function (result) {
-            // console.log(result);
-            $("#current").append("<p>" + result.name + " "+ todayDate + "___</p>")
+            $("#current").append("<p>" + result.name + " ("+ todayDate + ") ___</p>")
             $("#current").append("<p>Temperature: " + result.main.temp + " &#176F</p>")
             $("#current").append("<p>Humidity: " + result.main.humidity + "%</p>")
             $("#current").append("<p>Wind Speed: " + result.wind.speed + " MPH</p>")
-            $("#current").append("<p>UV Index: " + "___</p>")
+            
         });  
-        
+
+
+        var icons = "..assets/11d@2x.png";
+
+        function weatherIcon() {
+
+            if (res.cod >= 200 && res.cod <= 250) {
+                icons = "..assets/11d@2x.png";
+                // return icons
+            }
+        };
+
+        console.log(icons)
+
         $.ajax({
             url: queryURL_5Days,
             method: "GET",
         }).then(function (res) {
-            // console.log(res);
+            // console.log(res)
+            console.log(res.cod)
             // $(".five_forecast").append("<h1>5-Day Forecast:" + "</h1>");
-            $(".day_1").append("<p>"+ todayDate + "</p>")
-            $(".day_1").append("<p>img</p>")
-            $(".day_1").append("<p>Temp: " + res.list[0].main.temp + "&#176F</p>")
-            $(".day_1").append("<p>Humidity: " + res.list[0].main.humidity + "%</p>")
+            $("#day1").append("<h3>" + todayDate + "</h3>")
+            $("#day1").append("<img>" + icons +"</img>")
+            $("#day1").append("<p>Temp: " + res.list[0].main.temp + "&#176F</p>")
+            $("#day1").append("<p>Humidity: " + res.list[0].main.humidity + "%</p>")
         });
         
+        // $.ajax({
+        //     url: queryURL_UV,
+        //     method: "GET",
+        // }).then(function (res) {
+        //     console.log(res);
+        //     $("#current").append("<p>UV Index: " + res.value + "</p>")
+        // });
 
 
 
