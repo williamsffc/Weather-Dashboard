3cc255f8aceecae86ad3c318aa0fffeca447c253
will.i.am <67477328+williamsffc@users.noreply.github.com>
2020-08-14 08:45:41 -0700

    more updates

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ $(document).ready(function () {
         var key = "64b15ee1fcd8bf94f299ecb4565feacd";
         var todayDate = moment().format("M/DD/YYYY");
         var queryURl_current = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + key + "&units=imperial";
-        var queryURL_5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + key + "&units=imperial&cnt=5";
+        var queryURL_5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + key + "&units=imperial";
         // var queryURL_UV = "https://api.openweathermap.org/data/2.5/uvi?q=" + inputCity + "&appid=" + key + "&units=imperial";
 
+            if (inputCity !== '') { 
 
-        $("#listOutPut").append("<li>" + inputCity + "</li>")
-
-            if (inputCity != '') { 
+                $("#listOutPut").append("<li onclick='#'>" + inputCity + "</li>")
 
                 $.ajax({
                     url: queryURl_current,
                     method: "GET",
                 }).then(function (result) {
 
-                    $("#item1").append("<p>" + result.name + " (" + todayDate + ") ___</p>");
-                    $("#item2").append("<p>Temperature: " + result.main.temp + " &#176F</p>");
-                    $("#item3").append("<p>Humidity: " + result.main.humidity + "%</p>");
-                    $("#item4").append("<p>Wind Speed: " + result.wind.speed + " MPH</p>");
-                    $("#item5").append("<p>UV index: </p>");
+                    $("#item1").empty().append("<p>" + result.name + " (" + todayDate + ") ___</p>");
+                    $("#item2").empty().append("<p>Temperature: " + result.main.temp + " &#176F</p>");
+                    $("#item3").empty().append("<p>Humidity: " + result.main.humidity + "%</p>");
+                    $("#item4").empty().append("<p>Wind Speed: " + result.wind.speed + " MPH</p>");
+                    $("#item5").empty().append("<p>UV index: </p>");
 
                     $("#inputCity").val('');
+
                 }); 
                 
             } else {
@@ -39,11 +39,31 @@ $(document).ready(function () {
             url: queryURL_5Days,
             method: "GET",
         }).then(function (res) {
-            // $(".five_forecast").append("<h1>5-Day Forecast:" + "</h1>");
-            $("#day1").append("<h3>" + todayDate + "</h3>");
-            // $("#day1").append("<img>" + icons +"</img>")
-            $("#day1").append("<p>Temp: " + res.list[0].main.temp + "&#176F</p>");                              
-            $("#day1").append("<p>Humidity: " + res.list[0].main.humidity + "%</p>"); 
+            
+            $("#extended").empty();
+            for (var i = 0; i < res.list.length; i++) {
+
+                if (res.list[i].dt_txt.includes("09:00:00")){
+                    console.log(res.list[i])
+
+                    var displayDate = res.list[i].dt_txt.split(" ")[0];
+
+                    var newCard = $("<div>");
+
+                    newCard.addClass("card_5 forecast");
+                    newCard.append("<h3>" + displayDate + "</h3>");
+                    newCard.append("<p>Temp: " + res.list[i].main.temp + "&#176F</p>");
+                    newCard.append("<p>Humidity: " + res.list[i].main.humidity + "%</p>");
+                    
+                    $("#extended").append(newCard);
+                    // $("#day1").empty().append("<h3>" + displayDate + "</h3>");
+                    // // $("#day1").append("<img>" + icons +"</img>")
+                    // $("#day1").append("<p>Temp: " + res.list[i].main.temp + "&#176F</p>");
+                    // $("#day1").append("<p>Humidity: " + res.list[i].main.humidity + "%</p>");
+                }
+                
+            }
+             
         });
         
        
